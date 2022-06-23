afff98723d6a802f38c2ff1c7782987e7881ac07
Nazmi Sam <m.nazmi124@gmail.com>
2022-06-23 07:47:13 +0000

    Merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   draw :admin
   draw :user
-=======
-  resources :events
-  devise_for :admins
-  devise_for :users
-  root "dashboard#index"
-  get 'dashboard/index'
-  devise_for :admins, controllers: {
-  sessions: 'admins/sessions',
-  registrations: 'admins/registrations',
-  # confirmations: 'admins/confirmations'
-}
-  devise_for :users, controllers: {sessions: 'users/sessions'}
->>>>>>> 22e901a71801f022c1344c694460be207ca4a5df
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
