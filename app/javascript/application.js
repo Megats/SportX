6334f10548252ead211b0cefd6e43670c8c13558
exocode57 <mr.killer5757@gmail.com>
2022-06-28 02:52:41 +0000

    updated event_overview

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "trix"
+import "@rails/actiontext"
 $("#participantsTable").DataTable()
     pagingType: 'full_numbers'
 
@@ -9,4 +11,4 @@ $("#participantsTable").DataTable()
 //= require dataTables/jquery.dataTables
 //= require cocoon
 //= require dataTables/jquery.dataTablesimport "trix"
-import "@rails/actiontext"
+
