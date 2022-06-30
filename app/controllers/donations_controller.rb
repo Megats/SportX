301102363ff69b41908b5b7507d07437f7a64ff7
Nazmi Sam <m.nazmi124@gmail.com>
2022-06-30 07:57:36 +0000

    Updated landing page

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,6 +1,7 @@
 class DonationsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:derma]
   before_action :set_donation, only: %i[ show edit update destroy ]
-  before_action :get_collab
+  before_action :get_collab, except: [:derma]
 
   # GET /donations or /donations.json
   def index
@@ -35,7 +36,7 @@ class DonationsController < ApplicationController
         product_description: @donation.collab_id,
         transaction_amount: @donation.donation_amount,
         callback_url: "",
-        redirect_url: "",
+        redirect_url: "http://localhost:3000/users/collabs/#{@donation.collab_id}/donations/derma",
         token: "A64sFshdhzPmV5es_123",
         redirect_post: "true"
        }
@@ -59,7 +60,7 @@ class DonationsController < ApplicationController
   def update
     respond_to do |format|
       if @donation.update(donation_params)
-        format.html { redirect_to collab_donation_url(@donation), notice: "Donation was successfully updated." }
+        format.html { redirect_to user_collab_donation_url(@donation), notice: "Donation was successfully updated." }
         format.json { render :show, status: :ok, location: @donation }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -73,9 +74,13 @@ class DonationsController < ApplicationController
     @donation.destroy
 
     respond_to do |format|
-      format.html { redirect_to collab_donations_url, notice: "Donation was successfully destroyed." }
+      format.html { redirect_to user_collab_donations_url, notice: "Donation was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def derma
+      Rails.logger.debug "MEGAT DERMA #{params}"
   end
 
   private
@@ -83,6 +88,9 @@ class DonationsController < ApplicationController
       @collab = Collab.find(params[:collab_id])
     end
 
+    def get_user
+      @user = User.find(params[:user_id])
+    end
 
     # Use callbacks to share common setup or constraints between actions.
     def set_donation
@@ -91,6 +99,6 @@ class DonationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def donation_params
-      params.require(:donation).permit(:donation_name, :collab_id, :donation_email, :donation_number, :donation_nationality, :donation_COR, :donation_NRIC, :donation_dob, :donation_amount)
+      params.require(:donation).permit(:user_id, :donation_name, :collab_id, :donation_email, :donation_number, :donation_nationality, :donation_COR, :donation_NRIC, :donation_dob, :donation_amount)
     end
 end
