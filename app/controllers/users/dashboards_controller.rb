4c5431ff5bd11ed72edb067be5668e063a631bc2
Megats <dmegat14@gmail.com>
2022-07-01 11:13:57 +0800

    user dashboards

--- app/controllers/users/dashboards_controller.rb
+++ app/controllers/users/dashboards_controller.rb
@@ -2,9 +2,11 @@ class Users::DashboardsController < ApplicationController
   before_action :authenticate_user!
 
     def index
+      @event = Event.where(event_status: "OPEN").count
       @events = Event.all.count
-      @participants = Participant.all.count
       @collaborations = Collab.all.count
       @users = User.all.count
+      @numberparticipant = Participant.where(user_id: current_user.id).count
+      @participants = Participant.where(user_id: current_user.id)
     end
   end
