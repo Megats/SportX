2ef8622bae8e0ed297f09ac2a3fd9635609e9230
Megats <dmegat14@gmail.com>
2022-07-04 15:47:09 +0800

    update route letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
     resources :participants
     resources :results
   end
-
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
