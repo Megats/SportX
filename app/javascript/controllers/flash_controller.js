00542d2d0aadbca9b96eafa6e68dc7e237660f69
exocode57 <mr.killer5757@gmail.com>
2022-07-05 09:53:50 +0000

    updated flash message (haziq)

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.dismiss();
+    }, 2500);
+  }
+
+  dismiss() {
+    this.element.remove();
+  }
+}
